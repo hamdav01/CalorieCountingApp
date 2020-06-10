cbee247fc182f97672344e6f365054fd078951b1
Hampus Davidsson <hampus.davidsson@gmail.com>
2020-06-10 21:12:08 +0200

    added scroll view and lsit

--- App.js
+++ App.js
@@ -1,37 +1,88 @@
 import React from 'react';
-import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+  Text,
+  FlatList,
+  ScrollView,
+} from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 
-export default function App() {
-  
+function Item({ title }) {
   return (
-    <View style={styles.container}>
-      <TouchableOpacity
-        onPress={() => {
-          alert('in here');
-        }}
-        style={styles.button}
-      >
-        <AntDesign name='plus' size={24} color='black' />
-      </TouchableOpacity>
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
     </View>
+  );
+}
+export default function App() {
+  const [recipes, addRecipies] = React.useState([
+    {
+      text: `item: 0`,
+      id: 0,
+    },
+  ]);
+  return (
+    <SafeAreaView style={styles.container}>
+      <View styles={styles.header}>
+        <TouchableOpacity
+          onPress={() => {
+            addRecipies([
+              ...recipes,
+              {
+                text: `item: ${recipes.length}`,
+                id: recipes.length,
+              },
+            ]);
+          }}
+          style={styles.button}
+        >
+          <AntDesign name='plus' size={24} color='black' />
+        </TouchableOpacity>
+      </View>
+      <ScrollView>
+        <FlatList
+          style={styles.itemList}
+          data={recipes}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Item title={item.text} />}
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF',
+  header: {
     alignItems: 'flex-start',
-    justifyContent: 'flex-start',
+  },
+  container: {
+    backgroundColor: '#FFF',
+  },
+  itemList: {
+    marginHorizontal: 20,
+  },
+  item: {
+    marginTop: 5,
+    borderRadius: 5,
+    backgroundColor: '#f9c2ff',
+  },
+  title: {
+    padding: 10,
+    fontSize: 22,
+    color: '#000',
   },
   button: {
+    width: 55,
+
     borderWidth: 5,
     borderBottomLeftRadius: 20,
     borderBottomRightRadius: 20,
     padding: 10,
-    marginLeft: 10,
-    marginTop: 25,
+    marginLeft: 5,
+    marginTop: 5,
     borderRadius: 5,
   },
 });
