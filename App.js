e7904dcc804868172268906e1fc924d48ef2bd8c
Hampus Davidsson <hampus.davidsson@gmail.com>
2020-06-10 20:44:54 +0200

    add the recipe add button

--- App.js
+++ App.js
@@ -1,79 +1,18 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Platform,
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-} from 'react-native';
-import hampus from './assets/hampus.jpg';
-import * as ImagePicker from 'expo-image-picker';
-import * as Sharing from 'expo-sharing';
-import uploadToAnonymousFilesAsync from 'anonymous-files';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function App() {
-  const [selectedImage, setSelectedImage] = React.useState(null);
-  let openImagePickerAsync = async () => {
-    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
-
-    if (permissionResult.granted === false) {
-      alert('Permission to access camera roll is required!');
-      return;
-    }
-
-    let pickerResult = await ImagePicker.launchImageLibraryAsync();
-    if (pickerResult.cancelled === true) {
-      return;
-    }
-
-    if (Platform.OS === 'web') {
-      let remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri);
-      setSelectedImage({ localUri: pickerResult.uri, remoteUri });
-    } else {
-      setSelectedImage({ localUri: pickerResult.uri, remoteUri: null });
-    }
-  };
-
-  let openShareDialogAsync = async () => {
-    if (!(await Sharing.isAvailableAsync())) {
-      alert(
-        `The image is available for sharing at: ${selectedImage.remoteUri}`
-      );
-      return;
-    }
-
-    Sharing.shareAsync(selectedImage.localUri);
-  };
-
-  if (selectedImage !== null) {
-    return (
-      <View style={styles.container}>
-        <Image
-          source={{ uri: selectedImage.localUri }}
-          style={styles.thumbnail}
-        />
-        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
-          <Text style={styles.buttonText}>Share this photo</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-
+  
   return (
     <View style={styles.container}>
-      <Image source={hampus} style={styles.logo} />
-      <Image
-        source={{ uri: 'https://i.imgur.com/TkIrScD.png' }}
-        style={{ width: 305, height: 159 }}
-      />
-      <Text style={styles.instructions}>
-        To share a photo from your phone with a friend, just press the button
-        below!
-      </Text>
-
-      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
-        <Text style={styles.buttonText}>Pick a photo</Text>
+      <TouchableOpacity
+        onPress={() => {
+          alert('in here');
+        }}
+        style={styles.button}
+      >
+        <AntDesign name='plus' size={24} color='black' />
       </TouchableOpacity>
     </View>
   );
@@ -82,32 +21,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo: {
-    width: 305,
-    height: 159,
-    marginBottom: 10,
-  },
-  instructions: {
-    color: '#888',
-    fontSize: 18,
-    marginHorizontal: 15,
-  },
-  thumbnail: {
-    width: 300,
-    height: 300,
-    resizeMode: 'contain',
+    backgroundColor: '#FFF',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
   },
   button: {
-    backgroundColor: 'blue',
-    padding: 20,
+    borderWidth: 5,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    padding: 10,
+    marginLeft: 10,
+    marginTop: 25,
     borderRadius: 5,
-  },
-  buttonText: {
-    fontSize: 20,
-    color: '#fff',
   },
 });
